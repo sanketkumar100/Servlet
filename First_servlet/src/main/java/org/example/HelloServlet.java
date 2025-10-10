2c6e347be0a5f739a94b58aa61e274dd826142d7
sanketkumar100 <sanketkumar7645@gmail.com>
2025-10-11 04:06:23 +0530

    firsr servlet profram structure

---
+++ First_servlet/src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hello Guysss i am sanket, the best developer in the History</h1>");
+    }
+}
