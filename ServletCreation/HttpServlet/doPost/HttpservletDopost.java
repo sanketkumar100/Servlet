30d4282a0d918fc5f7c1141ea2d4b0698ea96e73
sanketkumar100 <sanketkumar7645@gmail.com>
2025-10-14 02:22:39 +0530

    using doPost method

---
+++ ServletCreation/HttpServlet/doPost/HttpservletDopost.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HttpservletDopost extends HttpServlet
+{
+   /* @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Please submit the form using POST method.</h2>");
+        out.println("</body></html>");
+    }*/
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        String username = req.getParameter("username");
+
+        out.println("<html><body>");
+        if (username != null && !username.isEmpty()) {
+            out.println("<h2>Hello, " + username + " from HttpServlet POST</h2>");
+        } else {
+            out.println("<h2>Hello without username from HttpServlet POST</h2>");
+        }
+        out.println("</body></html>");
+    }
+}
+
